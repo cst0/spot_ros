8718e4ddd0133787b0a35b4a49f7843037874aad
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-09-14 19:32:26 -0400

    expose new object/arm functionality to ros

--- spot_driver/src/spot_driver/arm/arm_wrapper.py
+++ spot_driver/src/spot_driver/arm/arm_wrapper.py
@@ -7,6 +7,7 @@ from std_srvs.srv import Trigger, TriggerResponse
 from spot_msgs.msg import OpenDoorAction
 from spot_msgs.srv import OpenDoor
 from vision_msgs.msg import Detection2D
+from spot_driver.arm.arm_utilities.object_grabber import object_grabber_main
 from spot_driver.arm.arm_utilities.door_opener import open_door_main
 
 from bosdyn.client.manipulation_api_client import ManipulationApiClient
@@ -52,12 +53,19 @@ class ArmWrapper:
             self.handle_gripper_close,
         )
 
+        self.grasp_point_userinput_srv = rospy.Service(
+            "grasp_point_userinput",
+            Trigger,
+            self.handle_grasp_point_userinput,
+        )
+
         dds = "door_detection_service"
         self.door_detection_service_proxy = None
         if rospy.has_param(dds):
             self.door_detection_service_proxy = rospy.ServiceProxy(
                 rospy.get_param(dds), Detection2D
             )
+        self.object_detection_service_proxy = None
 
         self._init_bosdyn_clients()
         self._init_actionservers()
@@ -99,4 +107,10 @@ class ArmWrapper:
         rospy.loginfo("Got a open door request")
         return open_door_main(
             self._robot, self._spot_wrapper, self.door_detection_service_proxy
-        )
+        ), "Complete!"
+
+    def handle_grasp_point_userinput(self, _):
+        rospy.loginfo("Got grasp point request (w/ user input)")
+        return object_grabber_main(
+            self._robot, self._spot_wrapper
+        ), "Complete!"
